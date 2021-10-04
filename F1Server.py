c26b9acf20f64f55d739f6d0a37fda1620e3ac28
giuliadelcitto <45166530+giuliadelcitto@users.noreply.github.com>
2021-10-04 17:10:27 +0200

    first commit

---
+++ F1Server.py
@@ -0,0 +1,260 @@
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, request
+import urllib.request, json, xmltodict
+import F1ServerFunctions
+import dbFunctions
+
+app = Flask(__name__)
+api = Api(app)
+
+
+PATH = '/'
+DATABASE = '/home/giulia/Desktop/Mobile/Proj/F1App/database.db'
+SCHEMA = '/home/giulia/Desktop/Mobile/Proj/F1App/db.sql'
+#IL DB SI RESETTA OGNI VOLTA ---> DA SISTEMARE POI
+
+class F1Server(Resource):
+
+    #ritorna elenco dei circuiti id,Nome,Nazione
+    @app.route(PATH+"circuits", methods=['GET'])
+    def get_circuits():
+        url = "http://ergast.com/api/f1/circuits"
+        response = urllib.request.urlopen(url)
+        my_xml = response.read()
+        my_dic = xmltodict.parse(my_xml)
+        circuit_list = my_dic['MRData']["CircuitTable"]['Circuit']
+
+        ret = []
+        circuit = ""
+        for c in circuit_list:
+            circuitId = c['@circuitId']
+            circuit = c['CircuitName'] + ',' + c['Location']['Country']
+            ret.append({'id': circuitId, "circuit":circuit})
+
+        return json.dumps(ret), 200
+   
+    #ritorna risultati ultimi 5 anni  e pole 
+    #query /circuit?name=nomedato
+    @app.route(PATH+"circuit", methods=['GET'])
+    def get_circuit():
+
+        circuitName = request.args.get('name').lower()
+        results = F1ServerFunctions.get_circuit_results(circuitName)
+        poles = F1ServerFunctions.get_circuit_poles(circuitName)
+        ret = {'results': results, 'poles':poles}
+
+        return ret
+
+    #ritorna elenco teams dal 2017 id,nome NB CI METTE UN BOTTO TIPO 6 SEC
+    @app.route(PATH+"teams", methods=['GET'])
+    def get_teams():
+        ret = []
+        for n in range(2017,2022): 
+            url = "http://ergast.com/api/f1/" + str(n) + "/constructors"
+            response = urllib.request.urlopen(url)
+            my_xml = response.read()
+
+            my_dic = xmltodict.parse(my_xml)
+            teams_list = my_dic['MRData']['ConstructorTable']['Constructor']
+        
+            for t in teams_list:
+                teamId = t['@constructorId']
+                teamName = t['Name']
+                team = {'teamId': teamId, 'teamName': teamName}
+                if team not in ret:
+                    ret.append(team)
+
+        return json.dumps(ret), 200
+    
+    #ritorna informazioni di un dato team
+    #query /team?name=nomedato
+    @app.route(PATH+"team", methods=['GET'])
+    def get_team():
+        teamName = request.args.get('name').lower()
+
+        nationality = F1ServerFunctions.get_team_nation(teamName)
+        currentDrivers = F1ServerFunctions.get_team_current_drivers(teamName)
+        standInfo = F1ServerFunctions.get_team_standInfo(teamName)
+        totalRaceWin = F1ServerFunctions.get_team_results(teamName)
+        totalChamWin = F1ServerFunctions.get_team_standings(teamName)
+
+        ret = {'nationality': nationality, 'currentDrivers' : currentDrivers, 'standInfo':standInfo, 'totalWinsRace': totalRaceWin, 'totalChampRace': totalChamWin}
+
+        return ret
+
+    #ritorna elenco piloti dal 2017 id,nome NB CI METTE UN BOTTO TIPO 6 SEC
+    @app.route(PATH+"drivers", methods=['GET'])
+    def get_drivers():
+        ret = []
+        for n in range(2017,2022): 
+            url = "http://ergast.com/api/f1/" + str(n) + "/drivers"
+            response = urllib.request.urlopen(url)
+            my_xml = response.read()
+
+            my_dic = xmltodict.parse(my_xml)
+            drivers_list = my_dic['MRData']['DriverTable']['Driver']
+        
+            for t in drivers_list:
+                driverId = t['@driverId']
+                driverName = t['GivenName']
+                driverSurname = t['FamilyName']
+                driver = {'driverId': driverId, 'driverName': driverName, 'driverSurname': driverSurname}
+                if driver not in ret:
+                    ret.append(driver)
+            
+        return json.dumps(ret), 200
+
+    #ritorna informazioni di un dato driver
+    #query /driver?name=nomedato
+    @app.route(PATH+"driver", methods=['GET'])
+    def get_driver():
+        driverName = request.args.get('name').lower()
+
+        driverInfo = F1ServerFunctions.get_driver_info(driverName)
+        wonraces = F1ServerFunctions.get_driver_wins(driverName)
+        wonchamp =  F1ServerFunctions.get_driver_champions(driverName)
+        poleraces = F1ServerFunctions.get_driver_pole(driverName)
+        currentStanding = F1ServerFunctions.get_driver_currentStanding(driverName)
+        teams = F1ServerFunctions.get_driver_teams(driverName)
+        ret = {'driverInfo': driverInfo, 'wonRaces':wonraces, 'poleRaces':poleraces, 'wonChamp':wonchamp, 'currentStanding':currentStanding, 'teamsList': teams}
+
+        return ret
+
+    #ritorna numero gara data luogo info su vincitore e pole man ULTIMA GARA
+    @app.route(PATH+"lastRace", methods=['GET'])
+    def get_lastRace(): 
+        url = "https://ergast.com/api/f1/current/last/results/1"
+        response = urllib.request.urlopen(url)
+        my_xml = response.read()
+        my_dic = xmltodict.parse(my_xml)
+
+        lastRace = my_dic['MRData']['RaceTable']['Race']
+        raceRound = lastRace['@round']
+        raceDate = lastRace['Date']
+        raceCircuit = lastRace['Circuit']['CircuitName']+ ',' + lastRace['Circuit']['Location']['Locality'] + ',' + lastRace['Circuit']['Location']['Country']
+        raceInfo = {'race': raceRound, 'raceDate':raceDate, 'raceCircuit': raceCircuit}
+
+        numb = lastRace['ResultsList']['Result']['@number']
+        driverId = lastRace['ResultsList']['Result']['Driver']['@driverId']
+        driverName = lastRace['ResultsList']['Result']['Driver']['GivenName']
+        driverSurname = lastRace['ResultsList']['Result']['Driver']['FamilyName']
+        constructor = lastRace['ResultsList']['Result']['Constructor']['Name']
+        winnerInfo = {'number': numb, 'driverId': driverId, 'driverName':driverName, 'driverSurname': driverSurname, 'constructor': constructor}
+
+        url = "https://ergast.com/api/f1/current/last/qualifying/1"
+        response = urllib.request.urlopen(url)
+        my_xml = response.read()
+        my_dic = xmltodict.parse(my_xml)
+
+        lastRacePole = my_dic['MRData']['RaceTable']['Race']
+
+        numb = lastRacePole['QualifyingList']['QualifyingResult']['@number']
+        driverId = lastRacePole['QualifyingList']['QualifyingResult']['Driver']['@driverId']
+        driverName = lastRacePole['QualifyingList']['QualifyingResult']['Driver']['GivenName']
+        driverSurname = lastRacePole['QualifyingList']['QualifyingResult']['Driver']['FamilyName']
+        constructor = lastRacePole['QualifyingList']['QualifyingResult']['Constructor']['Name']
+        PoleManInfo = {'number': numb, 'driverId': driverId, 'driverName':driverName, 'driverSurname': driverSurname, 'constructor': constructor}
+        
+        ret ={'raceInfo': raceInfo, 'winnerInfo': winnerInfo, 'PoleManInfo': PoleManInfo}
+        return json.dumps(ret), 200
+
+     #ritorna numero gara data luogo info su vincitore e pole man ULTIMA GARA
+   
+    #ritorna numero gara data ora luogo PROSSIMA GARA
+    @app.route(PATH+"NextRace", methods=['GET'])
+    def get_nextRace():
+        url = "https://ergast.com/api/f1/current/next"
+        response = urllib.request.urlopen(url)
+        my_xml = response.read()
+        my_dic = xmltodict.parse(my_xml)
+
+        lastRace = my_dic['MRData']['RaceTable']['Race']
+        raceRound = lastRace['@round']
+        raceDate = lastRace['Date']
+        raceTime = lastRace['Time']
+        raceCircuit = lastRace['Circuit']['CircuitName']+ ',' + lastRace['Circuit']['Location']['Locality'] + ',' + lastRace['Circuit']['Location']['Country']
+        raceInfo = {'race': raceRound, 'raceDate':raceDate, 'raceTime':raceTime + " Local time", 'raceCircuit': raceCircuit}
+
+        ret ={'raceInfo': raceInfo}
+        return json.dumps(ret), 200
+
+    
+    #DB Request
+
+    # Create a new favorite resource SISTEMARE TOKEN
+    @app.route(PATH+'createfavorite', methods=['POST'])
+    def createFavorite():
+        data = request.get_json()
+        if data:
+            print(data)
+            try:
+                # News verification
+                if 'news' in data:
+                    news = data['news']
+                    #da sistemare con token
+                    userid = data['userId']
+                    if 'id' in news and 'webPublicationDate' in news and 'webTitle' in news and 'webUrl' in news:
+                        resourceCreated = dbFunctions.insertFavorite(DATABASE, userid, news)
+                        if resourceCreated:
+                            print('Created favorite: ('+userid+', '+news['id']+')')
+                            return {'error': False}, 201
+                        else:
+                            # Duplicate
+                            return {'error': True}, 403
+                    else:
+                        # Bad request
+                        return {'error': True}, 400
+                else:
+                    # Bad request
+                    return {'error': True}, 400
+            except ValueError:
+                # Invalid tokenID or forbidden insertion
+                pass
+        return {'error': True}, 403
+
+    # Delete a favorite resource SISTEMA USER/TOKEN
+    @app.route(PATH+'deletefavorite', methods=['DELETE'])
+    def deleteFavorite():
+        parser = reqparse.RequestParser()
+        parser.add_argument('newsid', type=str, required=True)
+        args = parser.parse_args()
+        news = args['newsid']
+
+        userid = "1"
+
+        resourceDeleted = dbFunctions.deleteFavorite(DATABASE, userid, news)
+        if resourceDeleted:
+            print('Deleted favorite: ('+userid+', '+news+')')
+            return {'error': False}, 200
+        else:
+            # Not found
+            return {'error': True}, 404
+
+        return {'error': True}, 403
+
+
+    #only for debug
+    @app.route(PATH+'favorites', methods=['GET'])
+    def getFavorites():
+        connection = dbFunctions.get_db_connection(DATABASE)
+        userid = request.args.get('userId')
+        favorites = connection.execute("SELECT id, webPublicationDate, webTitle, webUrl FROM favorites JOIN news on content=id WHERE account='"+userid+"' ;").fetchall()
+        connection.commit()
+        connection.close()
+
+        res = []
+        for favorite in favorites:
+            favorite = dict(favorite)
+            res.append(favorite)
+
+        return {'favorites': res}, 200
+
+    
+api.add_resource(F1Server, PATH)
+
+if __name__=='__main__':
+    print('Initializing database...')
+    dbFunctions.init_db(DATABASE, SCHEMA)
+    print('Starting web server...')
+    app.run()
---
+++ F1ServerFunctions.py
@@ -0,0 +1,169 @@
+import urllib.request, json, xmltodict
+
+
+def get_team_nation(teamName): 
+    url = "https://ergast.com/api/f1/constructors/"+teamName
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    nationality = my_dic['MRData']['ConstructorTable']['Constructor']['Nationality']
+    return nationality
+
+def get_team_current_drivers(teamName):
+    url = "https://ergast.com/api/f1/current/constructors/"+ teamName +"/drivers"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+
+    drivers_list = my_dic['MRData']['DriverTable']['Driver']
+    currentDrivers= []
+    for t in drivers_list:
+        driverName = t['GivenName']
+        driverSurname = t['FamilyName']
+        driverNumb = t['PermanentNumber']
+        driver = {'permanentNumber':driverNumb, 'driverName': driverName, 'driverSurname': driverSurname}
+        currentDrivers.append(driver)
+    return currentDrivers
+
+def get_team_standInfo(teamName):
+    url = "https://ergast.com/api/f1/current/constructors/"+teamName+"/constructorStandings"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+
+    standInfo = "no current results"
+    standings = my_dic['MRData']['StandingsTable']
+    if my_dic['MRData']['@total'] != '0' :
+        rank = standings['StandingsList']['ConstructorStanding']['@position']
+        point = standings['StandingsList']['ConstructorStanding']['@points']
+        wins = standings['StandingsList']['ConstructorStanding']['@wins']
+        standInfo = {'rank':rank, 'points': point, 'wins':wins}
+    return standInfo
+
+def get_team_results(teamName):
+    url = "https://ergast.com/api/f1/constructors/"+teamName+"/results/1"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    totalRaceWin = my_dic['MRData']['@total']
+    return totalRaceWin
+
+def get_team_standings(teamName):
+    url = "https://ergast.com/api/f1/constructors/"+teamName+"/constructorStandings/1"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    totalChamWin = my_dic['MRData']['@total']
+    return totalChamWin
+
+def get_driver_info(driverName):
+    url = "https://ergast.com/api/f1/drivers/"+driverName
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+
+    driver = my_dic['MRData']['DriverTable']['Driver']
+    numb = driver['PermanentNumber']
+    name = driver['GivenName']
+    surname = driver['FamilyName']
+    birth = driver['DateOfBirth']
+    nationality = driver['Nationality']
+
+    driverInfo = {'numb':numb, 'name':name, 'surname':surname, 'birth':birth, 'nationality': nationality}
+
+    return driverInfo
+
+def get_driver_wins(driverName):
+    url = "https://ergast.com/api/f1/drivers/"+driverName+"/results/1"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    wonraces = my_dic['MRData']['@total']
+
+    return wonraces
+
+def get_driver_pole(driverName):
+    url = "https://ergast.com/api/f1/drivers/"+driverName+"/qualifying/1"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    poleraces = my_dic['MRData']['@total']
+    
+    return poleraces
+
+def get_driver_champions(driverName):
+    url = "https://ergast.com/api/f1/drivers/"+driverName+"/driverStandings/1"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    wonchamp = my_dic['MRData']['@total']
+    
+    return wonchamp
+
+def get_driver_teams(driverName):
+    url = "https://ergast.com/api/f1/drivers/"+driverName+"/constructors"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    teams_list = my_dic['MRData']['ConstructorTable']['Constructor']
+    teams = []
+    if my_dic['MRData']['@total'] != '1':
+        for t in teams_list:
+            name = t['Name']
+            nationality = t['Nationality']
+            team = {'name': name, 'nationality': nationality }
+            teams.append(team)
+    else : 
+        name = teams_list['Name']
+        nationality = teams_list['Nationality']
+        team = {'name': name, 'nationality': nationality }
+        teams.append(team)
+    return teams
+
+def get_driver_currentStanding(driverName):
+    url = "https://ergast.com/api/f1/current/drivers/"+driverName+"/driverStandings"
+    response = urllib.request.urlopen(url)
+    my_xml = response.read()
+    my_dic = xmltodict.parse(my_xml)
+    currentStanding = 'not in current championship'
+    if my_dic['MRData']['@total'] != '0':
+        standing = my_dic['MRData']['StandingsTable']['StandingsList']['DriverStanding']
+        position = standing['@position']
+        points =  standing['@points']
+        wins = standing['@wins']
+        team = standing['Constructor']['Name']
+        currentStanding = {'position':position, 'points':points, 'wins':wins, 'team':team}
+    return currentStanding
+
+def get_circuit_results(circuitName):
+    results = []
+    for n in range(2017,2022):
+        url = "https://ergast.com/api/f1/"+str(n)+"/circuits/"+circuitName+"/results/1"
+        response = urllib.request.urlopen(url)
+        my_xml = response.read()
+        my_dic = xmltodict.parse(my_xml)
+        dri = my_dic['MRData']['RaceTable']['Race']['ResultsList']['Result']
+        name = dri['Driver']['GivenName']
+        surname = dri['Driver']['FamilyName']
+        number = dri['Driver']['PermanentNumber']
+        team = dri['Constructor']['Name']
+        driver = {'name':name, 'surname':surname, 'number':number, 'team':team}
+        results.append(driver)
+    return results
+
+def get_circuit_poles(circuitName):
+    results = []
+    for n in range(2017,2022):
+        url = "https://ergast.com/api/f1/"+str(n)+"/circuits/"+circuitName+"/qualifying/1"
+        response = urllib.request.urlopen(url)
+        my_xml = response.read()
+        my_dic = xmltodict.parse(my_xml)
+        dri = my_dic['MRData']['RaceTable']['Race']['QualifyingList']['QualifyingResult']
+        name = dri['Driver']['GivenName']
+        surname = dri['Driver']['FamilyName']
+        number = dri['Driver']['PermanentNumber']
+        team = dri['Constructor']['Name']
+        time = dri['Q1']
+        driver = {'name':name, 'surname':surname, 'number':number, 'team':team, 'time':time}
+        results.append(driver)
+    return results
