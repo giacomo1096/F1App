8cd9b7a869a3486a073d5256819abb90af008001
giulia <giu.delci@gmail.com>
2021-10-08 13:11:24 +0200

    circuit info semi ok

--- F1App/app/src/main/java/com/example/f1app/ui/history/circuitsInfo.kt
+++ F1App/app/src/main/java/com/example/f1app/ui/history/circuitsInfo.kt
@@ -14,7 +14,9 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.f1app.R
 import kotlinx.android.synthetic.main.fragment_history.*
+import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 
 class circuitsInfo(circuitId:String) : Fragment() {
 
@@ -22,26 +24,59 @@ class circuitsInfo(circuitId:String) : Fragment() {
     private val url = "http://192.168.1.139:8000/circuit?name="+cId
     val jsonResponses: MutableList<String> = mutableListOf<String>()
 
-    fun createGrid(resultList: MutableList<String>, root: View){
-        val grid = root.findViewById(R.id.grid) as GridLayout
-        grid.removeAllViews()
+    fun estractData(grid : GridLayout, jsonArray: JSONArray){
 
-        val rowSize: Int = resultList.size //the number of bottons i have to put in GridLayout
-        var columnIndex = 0 //cols index to which i add the button
-        var rowIndex = 0
-        for (i in 0 until rowSize) {
-            val txtSrc = resultList[i]
-            val txt : TextView = TextView(context)
-            txt.text = txtSrc
-            rowIndex++ //here i increase the row index
-            columnIndex = 0
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(i)
+            val name : TextView = TextView(context)
+            val surname : TextView = TextView(context)
+            val numb : TextView = TextView(context)
+            val team : TextView = TextView(context)
+            val time : TextView = TextView(context)
 
-            val row = GridLayout.spec(rowIndex, 1)
-            val colspan = GridLayout.spec(columnIndex, 1)
-            val gridLayoutParam: GridLayout.LayoutParams = GridLayout.LayoutParams(row, colspan)
-            grid.addView(txt, gridLayoutParam)
-            grid.setVisibility(View.VISIBLE)
+            name.text = jsonObject.getString("name")
+            surname.text= jsonObject.getString("surname")
+            numb.text = jsonObject.getString("number")
+            team.text = jsonObject.getString("team")
+
+
+            val list : MutableList<TextView> = mutableListOf<TextView>()
+            list.add(numb)
+            list.add(name)
+            list.add(surname)
+            list.add(team)
+
+            if (jsonObject.length() == 5){
+                time.text = jsonObject.getString("time")
+                list.add(time)
+            }
+
+
+            for (j in 0 until list.size) {
+                val txt : TextView = list[j]
+                val row = GridLayout.spec(i, 1)
+                val colspan = GridLayout.spec(j, 1)
+                val gridLayoutParam: GridLayout.LayoutParams = GridLayout.LayoutParams(row, colspan)
+                grid.addView(txt, gridLayoutParam)
+                grid.setVisibility(View.VISIBLE)
+            }
         }
+
+    }
+
+    fun createGrid(response: JSONObject, root: View){
+
+        val gridPole = root.findViewById(R.id.gridPole) as GridLayout
+        gridPole.removeAllViews()
+
+        val gridResults = root.findViewById(R.id.gridResults) as GridLayout
+        gridPole.removeAllViews()
+
+        val jsonArrayPoles = response.getJSONArray("poles")
+        estractData(gridPole, jsonArrayPoles)
+        val jsonArrayResults = response.getJSONArray("results")
+        estractData(gridResults, jsonArrayResults)
+
 
     }
 
@@ -59,22 +94,11 @@ class circuitsInfo(circuitId:String) : Fragment() {
             Request.Method.GET, url, null,
             { response ->
                 try {
-                    val jsonArrayPoles = response.getJSONArray("poles")
-                    val jsonArrayResults = response.getJSONArray("results")
-                    for (i in 0 until jsonArrayPoles.length()) {
-                        val jsonObject = jsonArrayPoles.getJSONObject(i)
-                        //val id = jsonObject.getString("id")
-                        //val circuit_name = jsonObject.getString("circuit")
-                        //val item = mapOf(id to circuit_name)
-                        jsonResponses.add(jsonObject.toString())
-                    }
-                    createGrid(jsonResponses, itemView)
-
-                    Toast.makeText(context, "Response :$jsonResponses", Toast.LENGTH_LONG).show() //display the response on screen
-                } catch (e: JSONException) {
+                    createGrid(response, itemView)
+                    //Toast.makeText(context, "Response :$jsonResponses", Toast.LENGTH_LONG).show() //display the response on screen
+                 } catch (e: JSONException) {
                     e.printStackTrace()
                     Toast.makeText(context, "sei nel catch", Toast.LENGTH_LONG).show() //display the response on screen
-
                 }
             }) { error -> error.printStackTrace() }
 
